86bd3a7b8dd8742ec08e72beb521c0eb293bace2
laole918 <laole918@qq.com>
2021-10-14 18:41:12 +0800

    first commit

---
+++ XWAdSDKAdapterForSIG.framework/Headers/XWAdSDKAdapterForSIG.h
@@ -0,0 +1,18 @@
+//
+//  XWAdSDKAdapterForSIG.h
+//  XWAdSDKAdapterForSIG
+//
+//  Created by laole918 on 2021/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWAdSDKAdapterForSIG.
+FOUNDATION_EXPORT double XWAdSDKAdapterForSIGVersionNumber;
+
+//! Project version string for XWAdSDKAdapterForSIG.
+FOUNDATION_EXPORT const unsigned char XWAdSDKAdapterForSIGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWAdSDKAdapterForSIG/PublicHeader.h>
+
+
